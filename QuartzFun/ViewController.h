a49440f57bfb583e82ca7b1fc7ab962ca5774516
Alex Chekodanov <checkodanov@gmail.com>
2018-08-31 11:22:02 +0300

    Initial Commit

---
+++ QuartzFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuartzFun
+//
+//  Created by Alex Chekodanov on 31.08.2018.
+//  Copyright © 2018 MERA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
